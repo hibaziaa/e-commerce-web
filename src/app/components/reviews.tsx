84c665be54a3c2b8e5d27fd1665f2dc62d455dc1
hibaziaa <hzia10624@gmail.com>
2024-11-20 05:00:03 +0500

    addition of customer reviews section

---
+++ src/app/components/reviews.tsx
@@ -0,0 +1,16 @@
+export default function Reviews(){
+    return(
+        <>
+        <div>
+            <h1 className="font-bold md:text-5xl md:leading-[57.6px] leading-[36px] text-[32px] uppercase mt-12 mx-4 md:mt-24 md:mx-[100px] h-[72px] w-[286px] md:h-[56px]">
+                our happy customers
+            </h1>
+            <div className="flex justify-center items-center gap-4 mt-6 md:mt-14 mx-4 md:mx-[100px]">
+                <img src="/assets/reviewOne.png" alt="" className="w-[358px] md:w-[400px]"/>
+                  <img src="/assets/reviewTwo.png" alt="" className="hidden md:block md:w-[400px]  h-[240px"/>
+                  <img src="/assets/reviewThree.png" alt="" className="hidden md:block md:w-[400px] h-[240px]"/>
+            </div>
+        </div>
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Brands from "./components/brands";
 import Products from "./components/productShowcase";
 import TopSellingProducts from "./components/topSellingProducts";
 import Styles from "./components/styles";
+import Reviews from "./components/reviews";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Products />
       <TopSellingProducts/>
       <Styles />
+      <Reviews />
     </>
   );
 }
