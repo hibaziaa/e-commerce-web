4b09a58af267c8d4a31b3ba85ef6aca6b95d2e3c
hibaziaa <hzia10624@gmail.com>
2024-11-20 19:25:28 +0500

    adjusted margins

--- src/app/components/reviews.tsx
+++ src/app/components/reviews.tsx
@@ -2,7 +2,7 @@ export default function Reviews(){
     return(
         <>
         <div>
-            <h1 className="font-bold md:text-5xl md:leading-[57.6px] leading-[36px] text-[32px] uppercase mt-12 mx-4 md:mt-24 md:mx-[100px] h-[72px] w-[286px] md:h-[56px]">
+            <h1 className="font-bold md:text-5xl md:leading-[57.6px] leading-[36px] text-[32px] uppercase mt-12 mx-4 md:mt-20 md:mx-[100px] h-[72px] w-[286px] md:h-[56px] md:mb-20">
                 our happy customers
             </h1>
             <div className="flex justify-center items-center gap-4 mt-6 md:mt-14 mx-4 md:mx-[100px]">
