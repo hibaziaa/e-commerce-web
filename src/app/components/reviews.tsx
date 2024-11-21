817eeafccd7ee1f547483416b0eca9c2c6b9b98c
hibaziaa <hzia10624@gmail.com>
2024-11-22 02:27:57 +0700

    adjusted styling

--- src/app/components/reviews.tsx
+++ src/app/components/reviews.tsx
@@ -2,7 +2,7 @@ export default function Reviews(){
     return(
         <>
         <div>
-            <h1 className="font-bold md:text-5xl md:leading-[57.6px] leading-[36px] text-[32px] uppercase mt-12 mx-4 md:mt-20 md:mx-[100px] h-[72px] w-[286px] md:h-[56px] md:mb-20">
+            <h1 className="font-bold md:text-5xl md:leading-[57.6px] leading-[36px] text-[32px] uppercase mt-12 mx-4 md:mt-20 md:mx-[100px] h-[72px] w-[286px] md:h-[58px] md:mb-8 md:w-[654px]">
                 our happy customers
             </h1>
             <div className="flex justify-center items-center gap-4 mt-6 md:mt-14 mx-4 md:mx-[100px]">
